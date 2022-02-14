a63bc2fdfe0123ca8333571bc7369d0b2f9d35b6
Philip Garcia <philip_garcia1@baylor.edu>
2022-02-14 15:10:24 -0600

    quiz 1

--- processStudents.py
+++ processStudents.py
@@ -1,4 +1,4 @@
-''' 
+""" 
 *   Professor B would like to know which of his student have a GPA below 3.0.
     To accomplish this, read the file - students.csv into the program. The program
     should evaluate the GPA to see if it is higher or lower than 3.0. If it is,
@@ -17,62 +17,67 @@ not required to use them but it is provided to help you work
 through the logic of the problem.
 
 
-'''
+"""
 
 
 import csv
 
 
 # create a file object to open the file in read mode
-
+students = open("students.csv", "r")
 
 
 # create a csv object from the file object
+students_file = csv.reader(students, delimiter=",")
+
+# skip the header row
+next(students_file)
+
+# create an outfile object for the pocessed record
+outfile = open("processedStudents.csv", "w")
 
 
-#skip the header row
+# create a new dictionary named 'student_dict'
+student_dict = {}
 
 
-#create an outfile object for the pocessed record
-
-
-
-#create a new dictionary named 'student_dict'
-
-
-
-#use a loop to iterate through each row of the file
-
-    #check if the GPA is below 3.0. If so, write the record to the outfile
-    
-        
-
-
+# use a loop to iterate through each row of the file
+for record in students_file:
+    # check if the GPA is below 3.0. If so, write the record to the outfile
+    if record[8] in students_file >= 3.0:
+        outfile.write(
+            record[0]
+            + ","
+            + record[1]
+            + ","
+            + record[2]
+            + record[3]
+            + ","
+            + record[4]
+            + ","
+            + record[5]
+            + record[6]
+            + ","
+            + record[7]
+            + ","
+            + record[8]
+            + "\n"
+        )
 
     # append the record to the dictionary with the student id as the Key
+    student_dict = key(record[0])
     # and the value as the GPA
-    
+    student_dict = key(record[8])
+
+# print the entire dictionary
+print(student_dict)
 
 
+# Print the student id
+print(
+
+# print out the corresponding GPA from the dictionary
 
 
-
-#print the entire dictionary
-
-
-#Print the student id 
-
-
-#print out the corresponding GPA from the dictionary
-
-
-
-#close the outfile
-
-
-
-
-
-
-
-
+# close the outfile
+outfile.close()
